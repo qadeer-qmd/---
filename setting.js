a666540fcae8446142a3067781410d725edb304d
qadeer-qmd <qadeereditz360@gmail.com>
2026-02-11 22:33:15 +0500

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐐𝐀𝐃𝐄𝐄𝐑-𝐐𝐌𝐃',
+  author: '‎',
+  botName: "𝐐𝐀𝐃𝐄𝐄𝐑-𝐐𝐌𝐃",
+  botOwner: '𝐐𝐀𝐃𝐄𝐄𝐑', // Your name
+  ownerNumber: '923369423531', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/qadeer-qmd/---",
+};
+
+module.exports = settings;
